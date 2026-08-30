fd71d3b769abbe51aee293b8bb5c2cde621e15e5
vitoriamelo <Vitoriamelo.rdc@gmail.com>
2026-08-30 19:16:19 -0300

    Melhorando a classe de teste com setUp e tearDown

--- functional_tests.py
+++ functional_tests.py
@@ -4,10 +4,13 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
 
@@ -32,8 +35,6 @@ class NewVisitorTest(unittest.TestCase):
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
-
-        self.browser.quit()
 
 
 if __name__ == '__main__':
