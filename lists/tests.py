c5ddc079b19d36feee202814906875fe43a5094b
vitoriamelo <Vitoriamelo.rdc@gmail.com>
2026-09-03 21:46:20 -0300

    Corrigindo regressão

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.urls import resolve
-from django.test import TestCase
 from lists.views import home_page
+from django.test import TestCase
 
 
 class HomePageTest(TestCase):
@@ -11,9 +11,13 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post(
-        '/',
-        data={'item_text': 'A new list item'}
-    )
+            '/',
+            data={'item_text': 'A new list item'}
+        )
 
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_can_be_loaded_with_GET(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
