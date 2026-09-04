b1020aca256ae2b4a15aa0e29deafc7043a022e3
vitoriamelo <Vitoriamelo.rdc@gmail.com>
2026-09-03 21:31:47 -0300

    Processando o POST

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,13 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post(
+            '/',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertIn('A new list item', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+
+    return render(request, 'home.html')
