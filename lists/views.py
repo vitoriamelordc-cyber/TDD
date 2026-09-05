2f18624caf71428c5fe40c9bdc615780a15c3414
vitoriamelo <Vitoriamelo.rdc@gmail.com>
2026-09-05 19:52:35 -0300

    Salvando POST no banco

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
