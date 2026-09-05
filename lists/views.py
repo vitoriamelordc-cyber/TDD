bcb259b37ecfa0331705e2f7d1855ca7241aa3e0
vitoriamelo <Vitoriamelo.rdc@gmail.com>
2026-09-05 19:55:21 -0300

    Não salvando itens em branco

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,10 @@ from .models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
+        item_text = request.POST['item_text']
+
+        if item_text:
+            Item.objects.create(text=item_text)
 
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
